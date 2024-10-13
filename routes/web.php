39a55ab3362f1aecee5aa8175752f61eba8d0929
Marsella <220712081@students.uajy.ac.id>
2024-10-13 19:08:44 +0700

    PW6_E_12081

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('homepage');
+});
+Route::resource('/product', App\Http\Controllers\ProductController::class);
